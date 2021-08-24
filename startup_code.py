d440be9b4d6b663ac724d3ca205d6af657e32b28
gaius-r <gaiusreji11@gmail.com>
2021-08-24 12:36:00 +0530

    testing jenkins pollSCM

--- startup_code.py
+++ startup_code.py
@@ -1,3 +1,3 @@
 import joblib
 model = joblib.load("fiftystartups_model.pk1")
-model.predict([[165349.20, 137497.80, 476994.10, 1, 0]])
+print(model.predict([[165349.20, 137497.80, 476994.10, 1, 0]]))
